d1af3c0a6d7c1e966f6deb0f19567fac822c6231
laurelsumac <110875613+laurelsumac@users.noreply.github.com>
2022-09-04 18:33:15 -0700

    added buttons for player selection that lead to computer choice

--- cuttingedge.js
+++ cuttingedge.js
@@ -1,17 +1,50 @@
-
-
-
-
-function getComputerChoice(p1, p2) {
+const rock = document.getElementById("rock");
+rock.addEventListener("click", function() {
+    document.getElementById("playerChoice").textContent = "Rock";
     let compChoice = (Math.floor(Math.random() * 3) + 1);
 
     if (compChoice == 1) {
-        return 'Rock'
+        document.getElementById('computerChoice').textContent = 'Rock'
     } else if (compChoice == 2) {
-        return 'Paper'
+        document.getElementById('computerChoice').textContent = 'Paper'
     } else {
         return 'Scissors'
     }
+
+})
+
+const paper = document.getElementById("paper");
+paper.addEventListener("click", function() {
+    document.getElementById("playerChoice").textContent = "Paper";
+    let compChoice = (Math.floor(Math.random() * 3) + 1);
+
+    if (compChoice == 1) {
+        document.getElementById('computerChoice').textContent = 'Rock'
+    } else if (compChoice == 2) {
+        document.getElementById('computerChoice').textContent = 'Paper'
+    } else {
+        return 'Scissors'
+    }
+
+})
+
+const scissors = document.getElementById("scissors");
+scissors.addEventListener("click", function() {
+    document.getElementById("playerChoice").textContent = "Scissors";
+    let compChoice = (Math.floor(Math.random() * 3) + 1);
+    if (compChoice == 1) {
+        document.getElementById('computerChoice').textContent = 'Rock'
+    } else if (compChoice == 2) {
+        document.getElementById('computerChoice').textContent = 'Paper'
+    } else {
+        return 'Scissors'
+    }
+
+})
+
+// on click of one of the buttons, generate computer choice:
+function getComputerChoice() {
+
 }
 
-document.getElementById("demo").innerHTML = getComputerChoice();
+document.getElementById("computerChoice").innerHTML = getComputerChoice();
