828c06e8353b3067abd1be715003856732b69ba8
laurelsumac <110875613+laurelsumac@users.noreply.github.com>
2022-09-04 18:40:02 -0700

    corrected return 'scissors' to use getElementById

--- cuttingedge.js
+++ cuttingedge.js
@@ -1,44 +1,44 @@
 const rock = document.getElementById("rock");
-rock.addEventListener("click", function() {
-    document.getElementById("playerChoice").textContent = "Rock";
-    let compChoice = (Math.floor(Math.random() * 3) + 1);
+    rock.addEventListener("click", function() {
+        document.getElementById("playerChoice").textContent = "Rock";
+        let compChoice = (Math.floor(Math.random() * 3) + 1);
 
-    if (compChoice == 1) {
-        document.getElementById('computerChoice').textContent = 'Rock'
-    } else if (compChoice == 2) {
-        document.getElementById('computerChoice').textContent = 'Paper'
-    } else {
-        return 'Scissors'
-    }
+        if (compChoice == 1) {
+            document.getElementById('computerChoice').textContent = 'Rock'
+        } else if (compChoice == 2) {
+            document.getElementById('computerChoice').textContent = 'Paper'
+        } else {
+            document.getElementById('computerChoice').textContent = 'Scissors'
+        }
 
 })
 
 const paper = document.getElementById("paper");
-paper.addEventListener("click", function() {
-    document.getElementById("playerChoice").textContent = "Paper";
-    let compChoice = (Math.floor(Math.random() * 3) + 1);
+    paper.addEventListener("click", function() {
+        document.getElementById("playerChoice").textContent = "Paper";
+        let compChoice = (Math.floor(Math.random() * 3) + 1);
 
-    if (compChoice == 1) {
-        document.getElementById('computerChoice').textContent = 'Rock'
-    } else if (compChoice == 2) {
-        document.getElementById('computerChoice').textContent = 'Paper'
-    } else {
-        return 'Scissors'
-    }
+        if (compChoice == 1) {
+            document.getElementById('computerChoice').textContent = 'Rock'
+        } else if (compChoice == 2) {
+            document.getElementById('computerChoice').textContent = 'Paper'
+        } else {
+            document.getElementById('computerChoice').textContent = 'Scissors'
+        }
 
 })
 
 const scissors = document.getElementById("scissors");
-scissors.addEventListener("click", function() {
-    document.getElementById("playerChoice").textContent = "Scissors";
-    let compChoice = (Math.floor(Math.random() * 3) + 1);
-    if (compChoice == 1) {
-        document.getElementById('computerChoice').textContent = 'Rock'
-    } else if (compChoice == 2) {
-        document.getElementById('computerChoice').textContent = 'Paper'
-    } else {
-        return 'Scissors'
-    }
+    scissors.addEventListener("click", function() {
+        document.getElementById("playerChoice").textContent = "Scissors";
+        let compChoice = (Math.floor(Math.random() * 3) + 1);
+        if (compChoice == 1) {
+            document.getElementById('computerChoice').textContent = 'Rock'
+        } else if (compChoice == 2) {
+            document.getElementById('computerChoice').textContent = 'Paper'
+        } else {
+            document.getElementById('computerChoice').textContent = 'Scissors'
+        }
 
 })
 
